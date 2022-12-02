c7cffd9e1890f5ea51b2a25a294735336e22626f
X-CYL <castellastro@gmail.com>
2022-12-02 11:29:55 +0100

    create animation to 'charlescantinlogo' and test dropdown menu

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import menuButton from '../assets/pics/charte/icons8-menu-squared-96.png';
+import DefaultButton from './DefaultButton';
+
+const Dropdown = () => {
+    return (
+        <div className='menuContainer'>
+            <div className='menuTrigger'>
+                <img src = { menuButton } alt="bouton menu"></img>
+            </div>
+            <div classname ="dropdownmenu">
+                <ul>
+                    <DropdownItem />
+                </ul>
+
+            </div>
+            
+        </div>
+    );
+};
+
+function DropdownItem({label}){
+    return(
+        <DefaultButton 
+        label = "menu" />
+    );
+}
+
+export default Dropdown;
